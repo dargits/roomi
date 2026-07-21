135ee0977a4438dee75f8ce4fd61a3d6e1ba42bc
Dargit <imshydev@gmail.com>
2026-07-21 15:36:08 +0700

    future/price and room

--- src/main/java/roomi/dev/exception/ErrorCode.java
+++ src/main/java/roomi/dev/exception/ErrorCode.java
@@ -7,20 +7,20 @@ public class ErrorCode {
     public static final String INVALID_CREDENTIALS = "AUTH_003";
     public static final String SESSION_EXPIRED = "AUTH_004";
     public static final String SESSION_INVALID = "AUTH_005";
-    
+
     // Validation errors
     public static final String INVALID_INPUT = "VAL_001";
     public static final String MISSING_REQUIRED_FIELD = "VAL_002";
-    
+
     // Business logic errors
     public static final String ROOM_NOT_AVAILABLE = "ROOM_001";
     public static final String BOOKING_NOT_FOUND = "BOOK_001";
     public static final String INVALID_DATE_RANGE = "BOOK_002";
-    
+
     // System errors
     public static final String INTERNAL_ERROR = "SYS_001";
     public static final String DATABASE_ERROR = "SYS_002";
-    
+
     // Permission errors
     public static final String ACCESS_DENIED = "PERM_001";
     public static final String INSUFFICIENT_PRIVILEGES = "PERM_002";
@@ -35,5 +35,4 @@ public class ErrorCode {
     public static final String ROOM_TYPE_NOT_FOUND = "RATE_001";
     public static final String SEASONAL_RATE_NOT_FOUND = "RATE_002";
     public static final String DATE_RANGE_OVERLAP = "RATE_003";
-    public static final String INVALID_DATE_RANGE = "RATE_004";
 }
